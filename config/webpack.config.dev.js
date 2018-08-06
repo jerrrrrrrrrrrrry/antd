4947f34b855f446096f5cf2b1891f1a01753a61f
gitGujerry <632207717@qq.com>
2018-08-06 10:38:17 +0800

    <?xml version="1.0" encoding="UTF-8"?> <project version="4">   <component name="ProjectTasksOptions">     <TaskOptions isEnabled="true">       <option name="arguments" value="--no-cache --update $FileName$:$FileNameWithoutExtension$.css" />       <option name="checkSyntaxErrors" value="true" />       <option name="description" />       <option name="exitCodeBehavior" value="ERROR" />       <option name="fileExtension" value="scss" />       <option name="immediateSync" value="true" />       <option name="name" value="SCSS" />       <option name="output" value="$FileNameWithoutExtension$.css:$FileNameWithoutExtension$.css.map" />       <option name="outputFilters">         <array />       </option>       <option name="outputFromStdout" value="false" />       <option name="program" value="$PROJECT_DIR$/src/style/style.scss" />       <option name="runOnExternalChanges" value="true" />       <option name="scopeName" value="Project Files" />       <option name="trackOnlyRoot" value="true" />       <option name="workingDir" value="$FileDir$" />       <envs />     </TaskOptions>   </component> </project>

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -88,6 +88,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@': path.join(__dirname, '..', 'src'),
+
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
@@ -107,6 +109,7 @@ module.exports = {
 
       // First, run the linter.
       // It's important to do this before Babel processes the JS.
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'],},
       {
         test: /\.(js|jsx|mjs)$/,
         enforce: 'pre',
@@ -198,7 +201,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/,/.scss$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -94,6 +94,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@': path.join(__dirname, '..', 'src'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
@@ -113,6 +114,7 @@ module.exports = {
 
       // First, run the linter.
       // It's important to do this before Babel processes the JS.
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'],},
       {
         test: /\.(js|jsx|mjs)$/,
         enforce: 'pre',
@@ -222,7 +224,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/,/.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
