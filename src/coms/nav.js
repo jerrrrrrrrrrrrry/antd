91050a9b1e26df1af721a75b9e91b018ae855263
gitGujerry <632207717@qq.com>
2018-08-19 20:42:08 +0800

    <?xml version="1.0" encoding="UTF-8"?> <project version="4">   <component name="VcsDirectoryMappings">     <mapping directory="D:/react-antd" vcs="Git" />   </component> </project>

---
+++ src/coms/nav.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { Menu, Icon } from 'antd';
+import { Row, Col } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+export default class Nav extends Component {
+    state = {
+        current: 'mail',
+    }
+
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+            current: e.key,
+        });
+    }
+
+    render() {
+        return (
+
+
+
+            <Menu
+                onClick={this.handleClick}
+                selectedKeys={[this.state.current]}
+                mode="horizontal"
+            >
+                <Menu.Item key="mail">
+                    <Icon type="mail" />关于我们
+                </Menu.Item>
+                <Menu.Item key="app" disabled>
+                    <Icon type="appstore" />新闻资讯
+                </Menu.Item>
+                <SubMenu title={<span><Icon type="setting" />企业文化</span>}>
+                    <MenuItemGroup title="Item 1">
+                        <Menu.Item key="setting:1">Option 1</Menu.Item>
+                        <Menu.Item key="setting:2">Option 2</Menu.Item>
+                    </MenuItemGroup>
+                    <MenuItemGroup title="Item 2">
+                        <Menu.Item key="setting:3">Option 3</Menu.Item>
+                        <Menu.Item key="setting:4">Option 4</Menu.Item>
+                    </MenuItemGroup>
+                </SubMenu>
+                <Menu.Item key="alipay">
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+                </Menu.Item>
+            </Menu>
+        );
+    }
+}
--- src/page/site/index.js
+++ src/page/site/index.js
@@ -1,13 +1,12 @@
-// 我们需要在页面顶部，引用我们需要的各种工具
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-
+import Nav from '@/coms/nav.js'
 import Api from '@/tool/api.js'
 
-// 每一个 jsx 文件都得默认导出一个组件，格式如下
+// 这样，调用我们的自定义的组件
+import Header from '@/coms/header'
+
 export default class Index extends Component {
-    // 在这里，我们设置我们的初始数据，如，这里我们设置 list 为一个空数组
-    // 其他不用管，照抄，自己需要啥就写啥就可以了。
     constructor (props) {
         super(props)
         this.state = {
@@ -15,29 +14,19 @@ export default class Index extends Component {
         }
     }
 
-    // 当组件加载时，执行一些内容，其他时机执行，请搜索 react 生命周期
     componentDidMount () {
         this.getData()
     }
 
-    // 自定义一个方法，在其他地方用 this.方法名 来调用运行
     getData () {
         Api.get('topics', null, r => {
-            // react 和 vue 很大的不同就是 react 是单向绑定的。
-            // 所以，我们不能用直接等于的方法来更新数据
-            // 而是要用 setState 的方法更新数据
             this.setState({list: r.data})
         })
     }
 
-    // 每一个 jsx 组件，都必须包含 render 函数，这里渲染出我们的 dom 结构
     render () {
-        // 用 es6 的方式引用我们设置的数据
         let { list } = this.state
         let dom = null
-        // 下面这段代码比较恶心，尤其是之前学习 vue 的朋友更加觉得如此。
-        // 数据的循环，必须使用 .map 方式进行处理，然后 return 出来 dom 结构
-        // dom 结构用 () 包裹。单行时可以省略，但是不推荐省略
         if (list.length !== 0) {
             let listDom = list.map((i, k) => {
                 return (
@@ -50,9 +39,11 @@ export default class Index extends Component {
                 </div>
             )
         }
-        // 最后，把结果 return 出去，即可。
         return (
             <div className="outer home">
+                {/* 我们像用 html 标签一样，使用我们的自定义组件，并通过标签的方式，传值 */}
+                <Nav></Nav>
+                <Header title='网站首页'></Header>
                 {dom}
             </div>
         )
