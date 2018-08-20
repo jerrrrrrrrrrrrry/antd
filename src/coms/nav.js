96913bca068288de63d3b7306f3c2d0292175b6e
gitGujerry <632207717@qq.com>
2018-08-20 16:33:24 +0800

    <?xml version="1.0" encoding="UTF-8"?> <project version="4">   <component name="VcsDirectoryMappings">     <mapping directory="D:/react-antd" vcs="Git" />   </component> </project>

--- src/coms/nav.js
+++ src/coms/nav.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
-import { Row, Col } from 'antd';
-import { Layout, Menu, Breadcrumb , Icon } from 'antd';
+import React, {Component} from 'react'
+import {Row, Col} from 'antd';
+import {Layout, Menu, Breadcrumb, Icon} from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+const DemoBox = props => <p className={`height-${props.value}`}>{props.children}</p>;
+
 
 export default class Nav extends Component {
 
     state = {
-        current: 'index',
+        current: '',
     }
 
     handleClick = (e) => {
@@ -22,60 +24,62 @@ export default class Nav extends Component {
     render() {
         return (
 
-            <Layout className="layout" >
-                <Header>
-                    <div className="logo" />
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        onClick={this.handleClick}
-                        selectedKeys={[this.state.current]}
-                        style={{ lineHeight: '64px' }}
-                        subMenuCloseDelay={0.1}
-                    >
-                        <Menu.Item key="index">
-                            网站首页
-                        </Menu.Item>
-                        <SubMenu title={<span>关于我们</span>}>
+            <div className="nav">
+                <Row gutter={16} type="flex" justify="center">
+                    <Col span={1}></Col>
+                    <Col span={4}>
+                        <div className="logo"/>
+                    </Col>
+                    <Col span={14}>
+                        <Menu
+                            theme="light"
+                            mode="horizontal"
+                            onClick={this.handleClick}
+                            selectedKeys={[this.state.current]}
+                            style={{lineHeight: '80px'}}
+                            subMenuCloseDelay={0.1}
+                            className="navmenu"
+                        >
+                            <Menu.Item key="index">
+                                网站首页
+                            </Menu.Item>
+                            <SubMenu title={<span>关于我们</span>} className="gutter-box">
                                 <Menu.Item key="setting:1">公司简介</Menu.Item>
                                 <Menu.Item key="setting:2">组织机构</Menu.Item>
-                                <Menu.Divider />
                                 <Menu.Item key="setting:3">市场地位</Menu.Item>
                                 <Menu.Item key="setting:4">合作伙伴</Menu.Item>
-                        </SubMenu>
-                        <SubMenu title={<span>新闻资讯</span>}>
-                            <Menu.Item key="setting:5">企业新闻</Menu.Item>
-                            <Menu.Item key="setting:6">经营动态</Menu.Item>
-                        </SubMenu>
-                        <SubMenu title={<span>企业文化</span>}>
-                            <Menu.Item key="setting:7">企业荣誉</Menu.Item>
-                            <Menu.Item key="setting:8">发展前景</Menu.Item>
-                            <Menu.Item key="setting:9">团队荣誉</Menu.Item>
-                        </SubMenu>
-                        <SubMenu title={<span>服务与技术</span>}>
-                            <Menu.Item key="setting:7">服务功能</Menu.Item>
-                            <Menu.Item key="setting:8">解决方案</Menu.Item>
-                            <Menu.Item key="setting:9">信息技术</Menu.Item>
-                        </SubMenu>
-                        <SubMenu title={<span>营销中心</span>}>
-                            <Menu.Item key="setting:7">物流基地</Menu.Item>
-                            <Menu.Item key="setting:8">服务网络</Menu.Item>
-                            <Menu.Item key="setting:9">托盘共用</Menu.Item>
-                        </SubMenu>
-                        <SubMenu title={<span>联系方式</span>}>
-                            <Menu.Item key="setting:5">联系方式</Menu.Item>
-                            <Menu.Item key="setting:6">客户留言</Menu.Item>
-                        </SubMenu>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}></div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©2018 Created by Ant UED
-                </Footer>
-            </Layout>
+                            </SubMenu>
+                            <SubMenu title={<span>新闻资讯</span>} className="gutter-box">
+                                <Menu.Item key="setting:5">企业新闻</Menu.Item>
+                                <Menu.Item key="setting:6">经营动态</Menu.Item>
+                            </SubMenu>
+                            <SubMenu title={<span>企业文化</span>} className="gutter-box">
+                                <Menu.Item key="setting:7">企业荣誉</Menu.Item>
+                                <Menu.Item key="setting:8">发展前景</Menu.Item>
+                                <Menu.Item key="setting:9">团队荣誉</Menu.Item>
+                            </SubMenu>
+                            <SubMenu title={<span>服务与技术</span>} className="gutter-box">
+                                <Menu.Item key="setting:7">服务功能</Menu.Item>
+                                <Menu.Item key="setting:8">解决方案</Menu.Item>
+                                <Menu.Item key="setting:9">信息技术</Menu.Item>
+                            </SubMenu>
+                            <SubMenu title={<span>营销中心</span>} className="gutter-box">
+                                <Menu.Item key="setting:7">物流基地</Menu.Item>
+                                <Menu.Item key="setting:8">服务网络</Menu.Item>
+                                <Menu.Item key="setting:9">托盘共用</Menu.Item>
+                            </SubMenu>
+                            <SubMenu title={<span>联系方式</span>} className="gutter-box">
+                                <Menu.Item key="setting:5">联系方式</Menu.Item>
+                                <Menu.Item key="setting:6">客户留言</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Col>
+                    <Col span={4}></Col>
+                    <Col span={1}></Col>
+                </Row>
+            </div>
+
+
         );
     }
 }
--- src/page/site/index.js
+++ src/page/site/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import ReactDOM from 'react-dom';
 import Nav from '@/coms/nav.js'
 import Api from '@/tool/api.js'
 
 // 这样，调用我们的自定义的组件
 import Header from '@/coms/header'
+import Banner from "@/coms/banner";
 
 export default class Index extends Component {
     constructor (props) {
@@ -43,8 +45,9 @@ export default class Index extends Component {
             <div className="outer home">
                 {/* 我们像用 html 标签一样，使用我们的自定义组件，并通过标签的方式，传值 */}
                 <Nav></Nav>
-                <Header title='网站首页'></Header>
-                {dom}
+                <Banner/>
+                {/*<Header title='网站首页'></Header>
+                {dom}*/}
             </div>
         )
     }
